48d2532877f2736af9dcc2d3d4b93a907deebe76
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-10 20:26:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,9 +12,5 @@ def
 loop_message_n_times(message, number) 
 message = "Hello Moon."
 number = 5
-puts message  
-puts message  
-puts message  
-puts message  
-puts message  
+puts message * number 
 end
